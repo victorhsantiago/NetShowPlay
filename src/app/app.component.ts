e77e169f39f03a533e96535f65b9b643c2a7e8fc
Victor Santiago <victorhsantiago3@gmail.com>
2018-08-05 23:46:28 -0300

    few updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alurapic'
+  description = 'leão'
+  url = 'https://www.urbanarts.com.br/imagens/produtos/035728/0/Ampliada/leao-color.jpg'
 }
